e73da3cff95e05827f5308da43759aa6312bd89a
Muneersahel <saidmunir73@gmail.com>
2021-10-05 12:35:00 +0300

    Initite project depenedencies(flex, material, bootstrap and routing)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import {FlexLayoutModule} from "@angular/flex-layout";
+
+import {AccountsModule} from "./accounts/accounts.module";
+
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlexLayoutModule,
+    AccountsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
