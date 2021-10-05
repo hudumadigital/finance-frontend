df84cf1a05a061579dc26eafa7ece61200843e2b
Muneersahel <saidmunir73@gmail.com>
2021-10-05 18:07:22 +0300

    register page and validations of user inputs, added must-match validator

--- src/app/accounts/accounts.module.ts
+++ src/app/accounts/accounts.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {RouterModule} from "@angular/router";
+import {ReactiveFormsModule} from "@angular/forms";
+
 import { RegisterComponent } from './register/register.component';
-
-
 
 @NgModule({
   declarations: [
     RegisterComponent
   ],
-  imports: [
-    CommonModule
-  ]
+    imports: [
+      CommonModule,
+      RouterModule,
+      ReactiveFormsModule
+    ]
 })
 export class AccountsModule { }
--- src/app/accounts/register/register.component.ts
+++ src/app/accounts/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {MustMatch} from "../../helpers/must-match.validator";
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  passwordVisible = false;
+  passwordConfirmVisible = false;
+
+  registerForm!: FormGroup;
+  isSubmitted = false;
+
+  constructor(
+    private formBuilder: FormBuilder,) { }
 
   ngOnInit(): void {
+    this.registerForm = this.formBuilder.group({
+      'fullName': new FormControl(null, [Validators.required]),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'password': new FormControl(null, [Validators.required, Validators.min(8)]),
+      'passwordConfirm': new FormControl(null, [Validators.required])
+    }, {
+      validator: MustMatch('password', 'passwordConfirm')
+    });
   }
+
+  get f() {
+    return this.registerForm?.controls;
+  }
+
+  togglePasswordVisibility() {
+    this.passwordVisible = !this.passwordVisible;
+  }
+
+  togglePasswordConfirmVisibility() {
+    this.passwordConfirmVisible = !this.passwordConfirmVisible
+  }
+
+  onSubmit() {
+    this.isSubmitted = true;
+
+    if (this.registerForm?.invalid) {
+      return;
+    }
+
+    console.log(this.registerForm);
+
+  }
+
 
 }
